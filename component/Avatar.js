c139a55e4b8244109ab0bda86a60a3a53fcc701e
Kobe Puskas <Puskaskobe@gmail.com>
2023-01-22 15:41:44 -0500

    Added new components

---
+++ component/Avatar.js
@@ -0,0 +1,13 @@
+import { View, Text} from 'react-native';
+
+
+const Avatar = () => {
+
+    return (
+        <View>
+            <Text>Avatar</Text>
+        </View>
+    )
+}
+
+export default Avatar;
