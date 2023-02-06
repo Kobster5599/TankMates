a2deb49648f1923466d52a9cd6055f05a37932a7
Kobe Puskas <Puskaskobe@gmail.com>
2023-02-06 15:45:00 -0500

    working link

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import VideoPost from './component/VideoPost';
 import Avatars from './component/Avatar';
+import VideoPlayer from './component/VideoPlayer';
+import { WebLink } from './component/WebLink';
 
 export default function App() {
 
   const feedData = [
-    { id: 1, text: 'Post 1', videoLink: '', webLink: '', user: 'Kobe Puskas', avatar: '' },
+    { id: 1, text: 'Post 1', videoLink: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', webLink: 'https://google.com', user: 'Kobe Puskas', avatar: '' },
     { id: 2, text: 'Post 2', videoLink: '' },
     { id: 3, text: 'Post 3', videoLink: '' },
     { id: 4, text: 'Post 4', videoLink: '' },
@@ -26,7 +28,8 @@ export default function App() {
         <View style={styles.otherCol}>
            <Text style={styles.txtBold}>{item.user}</Text>
           <Text style={styles.txt}>{item.text}</Text>
-          <VideoPost />
+          { item.webLink !== '' ? <WebLink link={item.webLink} /> : ''}
+          { item.videoLink !== '' ? <VideoPlayer video={item.videoLink} /> : '' }
         </View>
         </View>
       );
